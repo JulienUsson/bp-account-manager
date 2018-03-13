93b2d32ee323e4251542ebac095f8cd5476502a3
Julien Usson <usson.julien@gmail.com>
2018-03-13 21:14:32 +0100

    :lipstick: import

--- src/scenes/Import.js
+++ src/scenes/Import.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import propTypes from 'prop-types'
 import Dropzone from 'react-dropzone'
-import { Typography } from 'material-ui'
+import { Typography, withStyles } from 'material-ui'
 import parseCsv from 'utils/parseCsv'
+
+const styles = theme => ({
+  dropzone: {
+    margin: 10,
+    borderRadius: 10,
+    border: '2px dashed #ffffff',
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: '2em',
+    fontWeight: 'bold',
+  },
+})
 
 class Import extends React.Component {
   onDrop = async file => {
@@ -11,15 +27,17 @@ class Import extends React.Component {
   }
 
   render() {
+    const { classes } = this.props
     return (
-      <Dropzone onDrop={this.onDrop}>
-        <Typography>DROP CSV FILE HERE !</Typography>
+      <Dropzone className={classes.dropzone} onDrop={this.onDrop}>
+        <Typography className={classes.text}>Drop CSV file here !</Typography>
       </Dropzone>
     )
   }
 }
 
 Import.propTypes = {
+  classes: propTypes.object.isRequired,
   onLoad: propTypes.func,
 }
 
@@ -27,4 +45,4 @@ Import.defaultValues = {
   onLoad: () => null,
 }
 
-export default Import
+export default withStyles(styles)(Import)
