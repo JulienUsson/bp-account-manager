1be8663d3f179245df0e2549d38af1217ed6eebd
Julien Usson <usson.julien@gmail.com>
2018-03-14 21:01:56 +0100

    :sparkles: format for google sheet

--- src/utils/copyToClipboard.js
+++ src/utils/copyToClipboard.js
@@ -1,5 +1,14 @@
 const { clipboard } = window.require('electron')
 
 export default data => {
-  clipboard.writeText('poc')
+  clipboard.writeText(
+    data
+      .map(line => {
+        const montant = line['Montant']
+        const label = line['Libellé']
+        const date = line['Date opération'].format('DD/MM/YY')
+        return `${montant}\t${label}\t${date}`
+      })
+      .join('\r\n')
+  )
 }
