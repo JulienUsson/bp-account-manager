51f7fc5f12f7c7f9a8bb456bca3748c4c20de5a1
Julien Usson <usson.julien@gmail.com>
2018-03-28 17:38:56 +0200

    :bug: use , and not . for number

--- src/utils/copyToClipboard.js
+++ src/utils/copyToClipboard.js
@@ -4,7 +4,7 @@ export default data => {
   clipboard.writeText(
     data
       .map(line => {
-        const montant = line['Montant']
+        const montant = String(line['Montant']).replace('.', ',')
         const label = line['Libellé']
         const date = line['Date opération'].format('DD/MM/YY')
         return `${montant}\t${label}\t${date}`
